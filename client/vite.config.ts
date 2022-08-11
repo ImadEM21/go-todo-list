410713600bc9b5ce03b1bbaa682d06ddfb455659
ImadEM21 <imad.elmahrad98@gmail.com>
2022-08-11 02:24:41 +0200

    update vite config for production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,19 @@ import react from 'vite-preset-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		sourcemap: false,
+		commonjsOptions: {
+			sourceMap: false,
+		},
+		minify: true,
+		rollupOptions: {
+			output: {
+			  manualChunks: {
+				editor: ['jodit'],
+				lodash: ['lodash'],
+			  },
+			},
+		  },
+	}
 });
