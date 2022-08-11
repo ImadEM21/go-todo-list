9f36eef3f083b9824b05cf737f3db6d9d51ca937
ImadEM21 <imad.elmahrad98@gmail.com>
2022-08-11 02:13:46 +0200

    update the roll options of vite for production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import react from 'vite-preset-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			maxParallelFileOps: 5
+		}
+	}
 });
