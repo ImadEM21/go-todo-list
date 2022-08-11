3d992da022a3a44dd38459bfccd2fa2be34782d6
ImadEM21 <imad.elmahrad98@gmail.com>
2022-08-11 02:19:38 +0200

    rollup vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,4 @@ import react from 'vite-preset-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			maxParallelFileOps: 5
-		}
-	}
 });
