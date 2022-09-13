61258f21fbc1403b3ab7d5ce4bdcddebebb149ce
ImadEM21 <imad.elmahrad98@gmail.com>
2022-09-13 15:32:55 +0200

    Server > Create a docker env for dev and prod

--- server/rebuild_docker.sh
+++ server/rebuild_docker.sh
@@ -1,3 +1,10 @@
 #!bin/bash
 
-docker-compose build --force-rm --no-cache
+export $(grep -v '^#' .env | xargs)
+
+if [ ${MODE} == "development" ];
+then
+    docker-compose build --force-rm --no-cache
+else
+    docker-compose -f docker-compose.prod.yaml build --force-rm --no-cache
+fi
